a7639fbae72fc2b6f9699f920aaf8199d43ce7fa
gh-charles <blakos244@gmail.com>
2023-09-21 04:29:14 -0500

    config secret key

--- umbrella/__init__.py
+++ umbrella/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+import os
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('UMBRELLA_SECRET_KEY')
 bcrypt = Bcrypt(app)
 
 from umbrella import routes
