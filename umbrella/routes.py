546c1d348dba233c9eb509b6e02efecf9853987f
gh-charles <blakos244@gmail.com>
2023-09-21 06:37:11 -0500

    add user auth redirects

--- umbrella/routes.py
+++ umbrella/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from umbrella import app, bcrypt
 from umbrella.forms import RegistrationForm, LoginForm
 from umbrella.models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 @app.route("/")
@@ -18,26 +18,34 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         hashed_password_str = hashed_password.decode('utf-8')
+
         if not form.bio:
             user = User(form.username.data, hashed_password_str, form.email.data)
         else:
             user = User(form.username.data, hashed_password_str, form.email.data, form.bio.data)
+
         user.insert_user_table()
         flash(form.username.data + ' account has been created.')
+        return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query_users(User(), ('email', form.email.data))
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             flash('You are now logged in.')
+            return redirect(url_for('home'))
         else:
             flash('Login was unsuccessful; please check your email and password.')
 
@@ -47,6 +55,7 @@ def login():
 @app.route("/logout")
 def logout():
     logout_user()
+    return redirect(url_for('home'))
 
 @app.route("/profile")
 @login_required
