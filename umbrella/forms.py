d2dbced6e63e180c5fc49b5d2dba37164ccca6a4
gh-charles <blakos244@gmail.com>
2023-09-21 02:42:00 -0500

    create RegistrationForm

--- umbrella/forms.py
+++ umbrella/forms.py
@@ -2,3 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
+    bio = StringField('Username', validators=[Length(min=2, max=150)])
+    submit = SubmitField('Sign up')
+
