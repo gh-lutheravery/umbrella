589982116104f04975a73460e0c6ba9470591245
gh-charles <blakos244@gmail.com>
2023-09-21 04:08:00 -0500

    add db insert for register

--- umbrella/routes.py
+++ umbrella/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from umbrella import app
+from umbrella import app, bcrypt
 from umbrella.forms import RegistrationForm, LoginForm
 from umbrella.models import User
 
@@ -12,3 +12,18 @@ def home():
 @app.route("/about")
 def about():
     return "<h1>About Page</h1>"
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        hashed_password_str = hashed_password.decode('utf-8')
+        if not form.bio:
+            user = User(form.username.data, hashed_password_str, form.email.data)
+        else:
+            user = User(form.username.data, hashed_password_str, form.email.data, form.bio.data)
+        user.insert_user_table()
+        flash(form.username.data + ' account has been created.')
+
+    return render_template('register.html', title='Register', form=form)
