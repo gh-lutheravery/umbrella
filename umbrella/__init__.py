cd484a4a88b7edde4ae4c26a3040b31b726fb86e
gh-charles <blakos244@gmail.com>
2023-09-21 05:18:00 -0500

    add login manager

--- umbrella/__init__.py
+++ umbrella/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('UMBRELLA_SECRET_KEY')
+
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from umbrella import routes
