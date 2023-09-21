bb4fa8115994189027f67ae8d14ec3bc1587629c
gh-charles <blakos244@gmail.com>
2023-09-21 02:33:40 -0500

    change app to use umbrella pkg

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return "<h1>Home Page</h1>"
-
-
-@app.route("/about")
-def about():
-    return "<h1>About Page</h1>"
-
+from umbrella import app
 
 if __name__ == '__main__':
     app.run(debug=True)
