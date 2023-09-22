4a27ca3d4c04ea3a07f439533d44365bd94edcfa
gh-charles <blakos244@gmail.com>
2023-09-22 03:21:56 -0500

    make formatted_query var

--- umbrella/db_interface.py
+++ umbrella/db_interface.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 import psycopg2
-import psycopg2.sql as SQL
+import psycopg2.sql as sql
 
 
 def open_conn():
@@ -20,13 +20,10 @@ def run_query(query: str, params=None, field_param=None):
     with open_conn() as conn:
         cursor = conn.cursor()
 
-        #new_params = [x.strip("'") for x in params]
-        #params_tuple = tuple(new_params)
-
-        #pinned tab
         if params:
             if field_param:
-                cursor.execute(SQL.SQL(query).format(SQL.Identifier(field_param)), params)
+                formatted_query = sql.SQL(query).format(sql.Identifier(field_param))
+                cursor.execute(formatted_query, params)
             else:
                 cursor.execute(query, params)
         else:
