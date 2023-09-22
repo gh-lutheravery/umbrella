c496929697d52ac3adab14a65929082ef093c4d3
gh-charles <blakos244@gmail.com>
2023-09-22 03:03:26 -0500

    make UpdateProfileForm

--- umbrella/forms.py
+++ umbrella/forms.py
@@ -1,14 +1,16 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 import umbrella.models as models
+
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')])
-    bio = StringField('Username', validators=[Length(min=2, max=150)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    bio = StringField('Bio', validators=[Length(min=2, max=250)])
     submit = SubmitField('Sign up')
 
     def validate_username(self, username):
@@ -30,3 +32,23 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
 
+
+class UpdateProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    bio = StringField('Bio', validators=[Length(min=2, max=250)])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = models.read_rows('profile', ('username', username))
+
+            if user:
+                raise ValidationError('An account with that username exists; choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = models.read_rows('profile', ('email', email))
+
+            if user:
+                raise ValidationError('An account with that email exists; choose a different one.')
