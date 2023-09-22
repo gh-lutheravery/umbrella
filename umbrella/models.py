7bc9ec7227afc37364a7be5b9d66179c2a7a9c77
gh-charles <blakos244@gmail.com>
2023-09-22 01:50:46 -0500

    make create_post_table

--- umbrella/models.py
+++ umbrella/models.py
@@ -199,7 +199,7 @@ class Post(DBModel, UserMixin):
 
     def query_posts(self, post_filter=None):
         if post_filter:
-            row = read_rows('profile', post_filter)
+            row = read_rows(self.table_name, post_filter)
 
             id, title, content, created_at, view_count, author_id, _ = row[0]
 
@@ -209,7 +209,7 @@ class Post(DBModel, UserMixin):
 
             return user
 
-        rows = read_rows('profile')
+        rows = read_rows(self.table_name)
         users = []
         for r in rows:
             id, title, content, created_at, view_count, author_id, _ = r
@@ -222,113 +222,17 @@ class Post(DBModel, UserMixin):
 
         return users
 
-    def read_user_rows(self, cond=None):
-        if cond:
-            query = "SELECT * FROM profile WHERE {} = %s AND is_deleted = False"
-            params = [cond[1]]
-            field_param = cond[0]
-            rows = db_interface.run_query(query, params, field_param)
-            return rows
-
-        query = "SELECT * FROM profile WHERE is_deleted = False"
-        rows = db_interface.run_query(query)
-        return rows
-
-    def query_users(self, user_filter=None):
-        if user_filter:
-            row = self.read_user_rows(user_filter)
-
-            user = User()
-
-            id, username, email, _, bio, join_date = row[0]
-
-            user.id = id
-            user.username = username
-            user.email = email
-            user.bio = bio
-            user.join_date = join_date
-
-            return user
-
-        rows = self.read_user_rows()
-        users = []
-        for r in rows:
-            user = User()
-            id, username, email, _, bio, join_date = r
-
-            user.id = id
-            user.username = username
-            user.email = email
-            user.bio = bio
-            user.join_date = join_date
-
-            users.append(user)
-
-        return users
-
-    def create_user_table(self):
-        query = "CREATE TABLE IF NOT EXISTS user (" \
-                "id, serial, PRIMARY KEY" \
-                "username, varchar(255), NOT NULL" \
-                "email, varchar(255), UNIQUE NOT NULL" \
-                "password, varchar(255), UNIQUE NOT NULL" \
-                "bio, varchar(511)" \
-                "created_at, timestamp, DEFAULT current_timestamp NOT NULL" \
-                "is_deleted, boolean" \
-                ");"
+    def create_post_table(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS post (
+            id serial PRIMARY KEY,
+            title varchar(255) UNIQUE NOT NULL,
+            content text NOT NULL,
+            created_at timestamp DEFAULT current_timestamp NOT NULL,
+            view_count bigint NOT NULL,
+            author_id varchar(255) UNIQUE NOT NULL,
+            is_deleted boolean
+        );
+        """
 
         db_interface.run_query(query)
-
-    def insert_user_table(self):
-        insert_query = """
-                INSERT INTO profile (id, username, email, password, bio, created_at, is_deleted)
-                VALUES (DEFAULT, %s, %s, %s, %s, %s, %s);
-                """
-
-        params = [
-            self.username,
-            self.email,
-            self.password,
-            self.bio,
-            self.join_date,
-            self.is_deleted
-        ]
-
-        db_interface.run_query(insert_query, params)
-
-    def update_user_table(self, soft_delete_flag=False):
-        if soft_delete_flag:
-            update_query = f"""
-                        UPDATE profile
-                        SET is_deleted = %s
-                        WHERE id = %s;
-                        """
-
-            params = [
-                self.is_deleted,
-                self.id
-            ]
-
-            db_interface.run_query(update_query, params)
-
-        else:
-            update_query = f"""
-                        UPDATE profile
-                        SET username = %s, email = %s, password = %s, bio = %s, created_at = %s, is_deleted = %s
-                        WHERE id = %s;
-                        """
-
-            params = [
-                self.username,
-                self.email,
-                self.password,
-                self.bio,
-                self.join_date,
-                self.is_deleted,
-                self.id
-            ]
-
-            db_interface.run_query(update_query, params)
-
-    def soft_delete(self):
-        self.update_user_table(soft_delete_flag=True)
