cd5ea8de12b191e2b1cbb82bc461f06b1e5208e2
gh-charles <blakos244@gmail.com>
2023-09-22 03:54:30 -0500

    add ckeditor

--- umbrella/__init__.py
+++ umbrella/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from  flask_ckeditor import CKEditor
 import os
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = os.getenv('UMBRELLA_SECRET_KEY')
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+ckeditor = CKEditor(app)
 
 from umbrella import routes
