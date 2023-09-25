1f822bd0c3939e09b3f554c77fda8f6f571ee1a4
gh-charles <blakos244@gmail.com>
2023-09-25 00:54:09 -0500

    add viewcounter

--- umbrella/__init__.py
+++ umbrella/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from  flask_ckeditor import CKEditor
+from flask_view_counter import ViewCounter
 import os
 
 app = Flask(__name__)
@@ -12,5 +13,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 app.config['CKEDITOR_PKG_TYPE'] = 'basic'
 ckeditor = CKEditor(app)
+
+view_counter = ViewCounter(app)
 
 from umbrella import routes
