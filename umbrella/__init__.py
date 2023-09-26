5ac57dfa1e2a2a1a4cff56803dbae4d5560465c2
gh-charles <blakos244@gmail.com>
2023-09-25 20:00:54 -0500

    delete viewcounter

--- umbrella/__init__.py
+++ umbrella/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from  flask_ckeditor import CKEditor
-from flask_view_counter import ViewCounter
 import os
 
 app = Flask(__name__)
@@ -14,6 +13,5 @@ login_manager.login_view = 'login'
 app.config['CKEDITOR_PKG_TYPE'] = 'basic'
 ckeditor = CKEditor(app)
 
-view_counter = ViewCounter(app)
 
 from umbrella import routes
